017594da2336f539df5a43207e7e336f276f550a
yulssem1010 <yulssem_nim@naver.com>
2024-11-28 12:39:54 +0900

    Delete portfolio 0718 folder

--- Desktop/최율희-공유폴더/포트폴리오/권태환/portfolio/ani/bear/js/move_custom.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-  setTimeout(function() {
-    window.location.href = 'sub2.html';
-  }, 3000);
-});
--- Desktop/최율희-공유폴더/포트폴리오/권태환/portfolio/clone/lgcom/js/main.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('.h_left button').click(function(){
-    $('.h_left button').toggleClass('on');
-    $('header .mobile_nav').toggle();
-  });
-});
